78b2af6c9a0c533869d1ecfbc89c5b202136f401
James Braza <jamesbraza@gmail.com>
2023-05-12 20:24:17 -0700

    Made GlobalConfig a dataclass to support configuration

--- baseline/config.py
+++ baseline/config.py
@@ -1,24 +1,35 @@
+import os
+from dataclasses import dataclass
+from typing import TypeAlias
+
 import numpy as np
 import torch
 
-from baseline import LOGS_FOLDER
+from baseline import BASELINE_FOLDER, LOGS_FOLDER
 from data import (
     BRATS_2020_TRAINING_FOLDER,
     BRATS_2020_TRAINING_VALIDATION_DATASET_FOLDER,
     BRATS_2020_VALIDATION_FOLDER,
 )
 
+PathLike: TypeAlias = str | os.PathLike
 
+
+@dataclass
 class GlobalConfig:
-    root_dir = BRATS_2020_TRAINING_VALIDATION_DATASET_FOLDER
-    train_root_dir = BRATS_2020_TRAINING_FOLDER
-    test_root_dir = BRATS_2020_VALIDATION_FOLDER
-    path_to_csv = "./train_data.csv"
-    pretrained_model_path = LOGS_FOLDER / "unet/last_epoch_model.pth"
-    train_logs_path = LOGS_FOLDER / "unet/train_log.csv"
-    ae_pretrained_model_path = LOGS_FOLDER / "ae/autoencoder_best_model.pth"
-    tab_data = LOGS_FOLDER / "data/df_with_voxel_stats_and_latent_features.csv"
-    seed = 55
+    root_dir: PathLike = BRATS_2020_TRAINING_VALIDATION_DATASET_FOLDER
+    train_root_dir: PathLike = BRATS_2020_TRAINING_FOLDER
+    test_root_dir: PathLike = BRATS_2020_VALIDATION_FOLDER
+    path_to_csv: PathLike = BASELINE_FOLDER / "train_data.csv"
+    pretrained_model_path: PathLike | None = LOGS_FOLDER / "unet/last_epoch_model.pth"
+    train_logs_path: PathLike = LOGS_FOLDER / "unet/train_log.csv"
+    ae_pretrained_model_path: PathLike | None = (
+        LOGS_FOLDER / "ae/autoencoder_best_model.pth"
+    )
+    tab_data: PathLike = (
+        LOGS_FOLDER / "data/df_with_voxel_stats_and_latent_features.csv"
+    )
+    seed: int = 55
 
 
 def seed_everything(seed: int):

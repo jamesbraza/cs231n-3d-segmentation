371ce7e9e3d5555237b1b738a9378be3b87da930
James Braza <jamesbraza@gmail.com>
2023-06-03 22:56:08 -0700

    Lightweight training and training docs (#8)

--- data/loaders.py
+++ data/loaders.py
@@ -1,8 +1,9 @@
-import itertools
 import os
+from enum import IntEnum
 
 import nibabel as nib
 import numpy as np
+import numpy.typing as npt
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
@@ -10,10 +11,47 @@ from torch.utils.data import Dataset
 from data import BRATS_2020_TRAINING_FOLDER, BRATS_2020_VALIDATION_FOLDER
 
 
+class BraTS2020Classes(IntEnum):
+    """
+    Classes found in the BraTS 2020 dataset.
+
+    SEE: https://www.med.upenn.edu/cbica/brats2020/data.html
+    """
+
+    NON_TUMOR = 0
+    NON_ENHANCING_TUMOR_CORE = 1  # Aka NCR/NET (neuroendocrine tumor)
+    # Swelling around the tumor
+    PERITUMORAL_EDEMA = 2  # Aka ED
+    # Gadolinium enhancing
+    GD_ENHANCING_TUMOR = 4  # Aka ET
+
+    @classmethod
+    def to_whole_tumor(cls, mask: npt.NDArray[int]) -> np.ndarray:
+        """Convert a mask to whole tumor (WT)."""
+        return mask >= cls.NON_TUMOR.value
+
+    @classmethod
+    def to_tumor_core(cls, mask: npt.NDArray[int]) -> np.ndarray:
+        """Convert a mask to tumor core (TC)."""
+        return np.logical_or(
+            mask == cls.NON_ENHANCING_TUMOR_CORE.value,
+            mask == cls.GD_ENHANCING_TUMOR.value,
+        )
+
+    @classmethod
+    def to_enhancing_tumor(cls, mask: npt.NDArray[int]) -> np.ndarray:
+        """Convert a mask to enhancing tumor (ET)."""
+        return mask == cls.GD_ENHANCING_TUMOR.value
+
+
 class BraTS2020Dataset(Dataset):
     """Map-style dataset for BraTS 2020."""
 
     TARGET_COLUMN = "BraTS_2020_subject_ID"
+    # flair = T2-weighted Fluid Attenuated Inversion Recovery (T2-FLAIR)
+    # t1 = native T1-weighted (T1)
+    # t1ce = post-contrast T1-weighted (T1Gd)
+    # t2 = T2-weighted (T2)
     NONMASK_EXTENSIONS = ["_flair.nii", "_t1.nii", "_t1ce.nii", "_t2.nii"]
     MASK_EXTENSION = "_seg.nii"
 
@@ -23,32 +61,52 @@ class BraTS2020Dataset(Dataset):
         mapping_csv_name: str,
         device: torch.device | None = None,
         train: bool = True,
+        skip_slices: int = 0,
     ):
-        self._data_folder_path = data_folder_path
+        """
+        Initialize.
+
+        Args:
+            data_folder_path: Path to the BraTS 2020 dataset.
+            mapping_csv_name: Name of the name mapping CSV file.
+            device: Optional torch device to use, default is None (CPU).
+            train: Set True (default) for training data (images and mask), set
+                False for test data (only images).
+            skip_slices: Symmetric count of MRI slices to exclude, since the
+                first few and last few slices usually are empty or nearly-empty.
+        """
+        self.data_folder_path = data_folder_path
         self._names = pd.read_csv(
             os.path.join(data_folder_path, mapping_csv_name),
             usecols=[self.TARGET_COLUMN],
             dtype=str,
         )
-        self._device = device
+        self.device = device
         self.train = train
+        self.skip_slices = skip_slices
 
     def __len__(self) -> int:
         return len(self._names)
 
     def get_full_path(self, index: int, extension: str) -> str:
         image_folder = os.path.join(
-            self._data_folder_path,
+            self.data_folder_path,
             self._names[self.TARGET_COLUMN][index],
         )
         return os.path.join(image_folder, os.path.basename(image_folder) + extension)
 
-    WT, TC, ET = 1, 2, 4
+    def _load_nii_with_slicing(self, path: str) -> np.ndarray:
+        """Load in a .nii file taking into account the skip slices."""
+        if self.skip_slices <= 0:
+            raw_img = nib.load(path).dataobj
+        else:
+            raw_img = nib.load(path).dataobj[:, :, self.skip_slices : -self.skip_slices]
+        return np.asarray(raw_img)
 
     def __getitem__(self, index: int) -> tuple[torch.Tensor, ...]:
         """Get (images, masks) if training, otherwise (images,)."""
         raw_imgs = (
-            np.asarray(nib.load(path).dataobj)
+            self._load_nii_with_slicing(path)
             for path in (
                 self.get_full_path(index, extension)
                 for extension in self.NONMASK_EXTENSIONS
@@ -60,21 +118,21 @@ class BraTS2020Dataset(Dataset):
             # N x W x H x C to N x C x H x W
             np.moveaxis(img, (0, 1, 2, 3), (0, 3, 2, 1)),
             dtype=torch.get_default_dtype(),  # Match pytorch-3dunet internals
-            device=self._device,
+            device=self.device,
         )
         if not self.train:
             return (image_tensor,)
-        mask = np.asarray(
-            nib.load(self.get_full_path(index, self.MASK_EXTENSION)).dataobj,
+        mask = self._load_nii_with_slicing(
+            path=self.get_full_path(index, self.MASK_EXTENSION),
         )
-        wt = mask >= self.WT
-        tc = np.logical_and(mask != self.TC, mask >= self.WT)
-        et = mask >= self.ET
+        wt = BraTS2020Classes.to_whole_tumor(mask)
+        tc = BraTS2020Classes.to_tumor_core(mask)
+        et = BraTS2020Classes.to_enhancing_tumor(mask)
         return image_tensor, torch.as_tensor(
             # N x W x H x C to N x C x H x W
             np.moveaxis(np.stack((wt, tc, et)), (0, 1, 2, 3), (0, 3, 2, 1)),
             dtype=torch.get_default_dtype(),  # Match pytorch-3dunet internals
-            device=self._device,
+            device=self.device,
         )
 
 
@@ -85,13 +143,16 @@ TRAIN_DS_KWARGS = {
 VAL_DS_KWARGS = {
     "data_folder_path": BRATS_2020_VALIDATION_FOLDER,
     "mapping_csv_name": "name_mapping_validation_data.csv",
+    "train": False,
 }
 
 
 def main() -> None:
     train_ds = BraTS2020Dataset(**TRAIN_DS_KWARGS)
+    for images, targets in train_ds:  # noqa: B007
+        _ = 0
     val_ds = BraTS2020Dataset(**VAL_DS_KWARGS)
-    for images, targets in itertools.chain(train_ds, val_ds):  # noqa: B007
+    for images in val_ds:  # noqa: B007
         _ = 0
 
 
--- unet_zoo/train.py
+++ unet_zoo/train.py
@@ -6,24 +6,58 @@ from pytorch3dunet.unet3d.model import UNet3D
 from pytorch3dunet.unet3d.trainer import UNetTrainer
 from pytorch3dunet.unet3d.utils import DefaultTensorboardFormatter
 from torch.utils.data import DataLoader
+from torchinfo import summary
 
 from data.loaders import TRAIN_DS_KWARGS, VAL_DS_KWARGS, BraTS2020Dataset
 from unet_zoo import ZOO_FOLDER
 
-NUM_SCANS_PER_EXAMPLE = 4
-NUM_CLASSES = 3
+NUM_SCANS_PER_EXAMPLE = len(BraTS2020Dataset.NONMASK_EXTENSIONS)
+MASK_COUNT = 3  # WT, TC, ET
 INITIAL_CONV_OUT_CHANNELS = 24
+SKIP_SLICES = 0
+BATCH_SIZE = 1
+
+
+def infer_device() -> torch.device:
+    if torch.cuda.is_available():
+        return torch.device("cuda")  # Current CUDA device
+    return torch.device("cpu")
+
+
+def print_summary(
+    model: torch.nn.Module,
+    skip_slices: int = SKIP_SLICES,
+    batch_size: int = BATCH_SIZE,
+) -> None:
+    num_slices = 155 - 2 * skip_slices
+    summary(model, input_size=(batch_size, NUM_SCANS_PER_EXAMPLE, num_slices, 240, 240))
+
 
 model = UNet3D(
     in_channels=NUM_SCANS_PER_EXAMPLE,
-    out_channels=NUM_CLASSES,
+    out_channels=MASK_COUNT,
     final_sigmoid=True,
     f_maps=INITIAL_CONV_OUT_CHANNELS,
     num_groups=6,
 )
+# print_summary(model)
 data_loaders: dict[Literal["train", "val"], DataLoader] = {
-    "train": DataLoader(BraTS2020Dataset(**TRAIN_DS_KWARGS)),
-    "val": DataLoader(BraTS2020Dataset(**VAL_DS_KWARGS)),
+    "train": DataLoader(
+        BraTS2020Dataset(
+            device=infer_device(),
+            skip_slices=SKIP_SLICES,
+            **TRAIN_DS_KWARGS,
+        ),
+        batch_size=BATCH_SIZE,
+    ),
+    "val": DataLoader(
+        BraTS2020Dataset(
+            device=infer_device(),
+            skip_slices=SKIP_SLICES,
+            **VAL_DS_KWARGS,
+        ),
+        batch_size=BATCH_SIZE,
+    ),
 }
 trainer = UNetTrainer(
     model,
@@ -34,7 +68,8 @@ trainer = UNetTrainer(
     loaders=data_loaders,
     checkpoint_dir=str(ZOO_FOLDER / "logs"),
     max_num_epochs=5,
-    max_num_iterations=1000,  # Max number of batches per epoch
+    max_num_iterations=1000,  # Defeat max number of batches per epoch
     tensorboard_formatter=DefaultTensorboardFormatter(),
+    skip_train_validation=True,
 )
 trainer.fit()

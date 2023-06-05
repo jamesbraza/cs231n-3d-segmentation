c3a5bff529e596328a39780866d8500f75ba4f3e
James Braza <jamesbraza@gmail.com>
2023-06-04 20:19:27 -0700

    Moved to main structure to import from train

--- unet_zoo/train.py
+++ unet_zoo/train.py
@@ -30,41 +30,46 @@ def print_summary(
     summary(model, input_size=(batch_size, NUM_SCANS_PER_EXAMPLE, num_slices, 240, 240))
 
 
-model = UNet3D(
-    in_channels=NUM_SCANS_PER_EXAMPLE,
-    out_channels=MASK_COUNT,
-    final_sigmoid=True,
-    f_maps=INITIAL_CONV_OUT_CHANNELS,
-    num_groups=9,
-).to(device=infer_device())
-# print_summary(model)
+def main() -> None:
+    model = UNet3D(
+        in_channels=NUM_SCANS_PER_EXAMPLE,
+        out_channels=MASK_COUNT,
+        final_sigmoid=True,
+        f_maps=INITIAL_CONV_OUT_CHANNELS,
+        num_groups=9,
+    ).to(device=infer_device())
+    # print_summary(model)
 
-train_val_ds = BraTS2020Dataset(
-    device=infer_device(),
-    skip_slices=SKIP_SLICES,
-    **TRAIN_VAL_DS_KWARGS,
-)
-train_ds, val_ds = split_train_val(train_val_ds, batch_size=BATCH_SIZE)
+    train_val_ds = BraTS2020Dataset(
+        device=infer_device(),
+        skip_slices=SKIP_SLICES,
+        **TRAIN_VAL_DS_KWARGS,
+    )
+    train_ds, val_ds = split_train_val(train_val_ds, batch_size=BATCH_SIZE)
 
-data_loaders: dict[Literal["train", "val"], DataLoader] = {
-    "train": DataLoader(train_ds, batch_size=BATCH_SIZE),
-    "val": DataLoader(val_ds, batch_size=BATCH_SIZE),
-}
-defeat_max_num_iters = (
-    NUM_EPOCHS * max(len(data_loaders[split]) for split in data_loaders) + 1
-)
+    data_loaders: dict[Literal["train", "val"], DataLoader] = {
+        "train": DataLoader(train_ds, batch_size=BATCH_SIZE),
+        "val": DataLoader(val_ds, batch_size=BATCH_SIZE),
+    }
+    defeat_max_num_iters = (
+        NUM_EPOCHS * max(len(data_loaders[split]) for split in data_loaders) + 1
+    )
 
-optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
-trainer = UNetTrainer(
-    model,
-    optimizer=optimizer,
-    lr_scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer),
-    loss_criterion=BCEDiceLoss(alpha=1.0, beta=1.0),
-    eval_criterion=MeanIoU(),
-    loaders=data_loaders,
-    checkpoint_dir=str(CHECKPOINTS_FOLDER),
-    max_num_epochs=NUM_EPOCHS,
-    max_num_iterations=defeat_max_num_iters,
-    tensorboard_formatter=DefaultTensorboardFormatter(),
-)
-trainer.fit()
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
+    trainer = UNetTrainer(
+        model,
+        optimizer=optimizer,
+        lr_scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer),
+        loss_criterion=BCEDiceLoss(alpha=1.0, beta=1.0),
+        eval_criterion=MeanIoU(),
+        loaders=data_loaders,
+        checkpoint_dir=str(CHECKPOINTS_FOLDER),
+        max_num_epochs=NUM_EPOCHS,
+        max_num_iterations=defeat_max_num_iters,
+        tensorboard_formatter=DefaultTensorboardFormatter(),
+    )
+    trainer.fit()
+
+
+if __name__ == "__main__":
+    main()
